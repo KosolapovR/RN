75830a1a20395536d88934283d646d148d99658b
ROMAN KOSOLAPOV <roman.kosolapov@cashelec.ru>
2021-03-18 09:40:11 +0300

    Setup shared navigation element

--- src/components/forms/Connection2faForm/index.js
+++ src/components/forms/Connection2faForm/index.js
@@ -1,73 +1,66 @@
 import React, {useCallback, useState} from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
 
 import BasicButton from 'components/buttons/BasicButton';
-import {PrimaryText} from 'components/styled';
-import AppIcon from 'assets/img/2fa-mobile.svg';
-import AppSelectedIcon from 'assets/img/2fa-active.svg';
-import TelegramIcon from 'assets/img/new-settings/telegram-grey.svg';
-import TelegramSelectedIcon from 'assets/img/new-settings/telegram-active.svg';
-import Selected2faBlock from 'components/blocks/Selected2faBlock';
+import {PrimaryBoldLargeText} from 'components/styled';
+import RadioButtonRN from 'radio-buttons-react-native';
+import {useTheme} from 'styled-components';
 
 const StyledForm = styled.View`
   flex: 1;
 `;
 
 const StyledButtonsWrapper = styled.View`
-  margin-top: 20px;
-  height: 100px;
-  justify-content: space-between;
+  margin-top: 25px;
 `;
 
-const items = [
-  {
-    id: 1,
-    title: '2FA App',
-    subtitle: 'Authy/Google Authenticator',
-    icon: <AppIcon height={25} width={25} />,
-    isSelected: true,
-    selectedIcon: <AppSelectedIcon height={25} width={25} />,
-  },
-  {
-    id: 2,
-    title: 'Telegram',
-    subtitle: 'Push-уведомления',
-    icon: <TelegramIcon height={23} width={23} />,
-    isSelected: false,
-    selectedIcon: <TelegramSelectedIcon height={23} width={23} />,
-  },
-];
+const Connect2faForm = ({onSubmit}) => {
+  const theme = useTheme();
 
-const Connection2faForm = ({onSubmit, onSkip}) => {
-  const [selected2FA, setSelected2FA] = useState();
+  const [is2fa, set2FA] = useState(true);
 
-  const onSelect = (id) => {
-    setSelected2FA(id);
-    items.forEach((item) => {
-      item.isSelected = item.id === id;
-    });
-  };
-
-  const onContinue = useCallback(() => onSubmit(selected2FA), [selected2FA]);
+  const onContinue = useCallback(() => {
+    onSubmit(is2fa);
+  }, [is2fa]);
 
   return (
     <StyledForm>
-      <PrimaryText paddingBottom={20}>
-        Выберите способ подключения двухфакторной аутентификации:
-      </PrimaryText>
-      <Selected2faBlock onSelect2FA={onSelect} items={items} />
+      <PrimaryBoldLargeText paddingBottom={15}>
+        Подключение 2FA
+      </PrimaryBoldLargeText>
+      <RadioButtonRN
+        initial={1}
+        boxStyle={{
+          height: 45,
+          padding: 0,
+          backgroundColor: theme.main.backgroundColors.primary,
+          borderWidth: 0,
+        }}
+        activeColor={theme.main.colors.blue}
+        deactiveColor={theme.main.colors.primary}
+        animationTypes={['shake']}
+        textColor={theme.main.colors.primary}
+        data={[
+          {
+            label: 'Я хочу подключить 2FA сейчас',
+            value: true,
+          },
+          {
+            label: 'Пропустить и подключить позже',
+            value: false,
+          },
+        ]}
+        selectedBtn={(e) => {
+          set2FA(e.value);
+        }}
+      />
       <StyledButtonsWrapper>
         <BasicButton color="primary" title="Продолжить" onClick={onContinue} />
-        <BasicButton title="Не подключать 2FA" onClick={onSkip} />
       </StyledButtonsWrapper>
     </StyledForm>
   );
 };
 
-Connection2faForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-  onSkip: PropTypes.func.isRequired,
-};
+Connect2faForm.propTypes = {};
 
-export default Connection2faForm;
+export default Connect2faForm;
--- src/components/forms/Select2faForm/Select2faForm.stories.js
+++ src/components/forms/Select2faForm/Select2faForm.stories.js
@@ -2,13 +2,13 @@ import {storiesOf} from '@storybook/react-native';
 import CenterView from '../../../../storybook/stories/CenterView';
 import {action} from '@storybook/addon-actions';
 import React from 'react';
-import Connection2faForm from './index';
+import Select2faForm from './index';
 
 storiesOf('Формы', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
-  .add('Форма с возможностью подключить 2FA', () => (
-    <Connection2faForm
-      handleSubmit={action('handleSubmit')}
-      onEnterWithRecoveryCodes={action('onEnterWithRecoveryCodes')}
+  .add('Форма выбора варианта 2FA', () => (
+    <Select2faForm
+      onSubmit={action('onSubmit')}
+      onSkip={action('onSkip')}
     />
   ));
